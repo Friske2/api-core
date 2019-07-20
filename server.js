8ac9e697f5df7a9df49dca42376610137fd7b87a
dainokung <dainokung2535@gmail.com>
2019-07-20 23:43:14 +0700

    setup body-parser access-control

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 import fs from 'fs'
 
 const app = express()
 const PORT = process.env.PORT || 3000;
+
 // setup route 
 const APP_DIR = `${__dirname}/src/routes`
 const features = fs.readdirSync(APP_DIR).filter(
@@ -15,6 +17,18 @@ features.forEach(features => {
     routes.setup(router)
     app.use(`/${path}`, router)
 })
+
+// setup bodyParser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// setup Access-Control
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  next();
+});
 
 app.listen(PORT,()=>{
   console.log(`server start port ${PORT}`)
