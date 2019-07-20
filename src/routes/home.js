868649cd12edda39e5d977ae3d7307adb7590502
dainokung <dainokung2535@gmail.com>
2019-07-20 23:57:05 +0700

    check params

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,4 +2,7 @@ export function setup(router) {
     router.get('/',(req,res)=>{
         res.send('hello')
     })
+    router.get('/:id',(req,res)=>{
+        res.send('hello : '+req.params.id)
+    })
 }
