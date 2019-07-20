86af0d0127b4037011a0cea2866b0e80fce2c6d6
dainokung <dainokung2535@gmail.com>
2019-07-21 00:34:37 +0700

    setup mongodb

---
+++ connect.js
@@ -0,0 +1,12 @@
+import mongoos from 'mongoose'
+import config from './config'
+
+let {username,password,baseUrl,port,dbName} = config
+mongoos.connect(`mongodb://${username}:${password}@${baseUrl}:${port}/${dbName}`, { useNewUrlParser: true });
+mongoos.connection
+.once('open', () => {
+    console.log('connect mongodb complete')
+})
+.on('error',(error) => {
+    console.warn('Warning',error)
+})
