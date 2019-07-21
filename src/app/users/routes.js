102f2380c6502159da258cf07196d14471697239
dainokung <dainokung2535@gmail.com>
2019-07-21 10:09:28 +0700

    edit root folder

---
+++ src/app/users/routes.js
@@ -0,0 +1,8 @@
+import controller from './controller';
+export function setup(router) {
+  router.get('/', controller.getAll);
+  router.get('/:id', controller.selected);
+  router.post('/', controller.insert);
+  router.put('/', controller.update);
+  router.delete('/:id', controller.delete);
+}
