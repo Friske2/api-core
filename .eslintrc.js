303b376a61f337926a47e1b9bc9f80a1d3f57573
Jellopy <parnupat.j@gmail.com>
2020-09-22 22:39:48 +0700

    set lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-    env: {
-       "jest": true
-    },
-    // Standard JavaScript Style Guide
-    extends: ["standard", "plugin:prettier/recommended"],
-    rules: {
-       "prettier/prettier": ["error", { "singleQuote": true }]
-    }
- };
+  env: {
+    jest: true
+  },
+  // Standard JavaScript Style Guide
+  extends: ['standard', 'plugin:prettier/recommended'],
+  rules: {
+    'prettier/prettier': ['error', { singleQuote: true }]
+  }
+};
