9355e2144088663bc47b8f1ee26adda66790b4c6
Jellopy <parnupat.j@gmail.com>
2020-09-22 22:40:07 +0700

    connect mongo db

--- config/index.js
+++ config/index.js
@@ -1,9 +1,9 @@
 const config = {
-    username:'admin',
-    password:'secure',
-    baseUrl:'128.199.192.85',
-    port:'2277',
-    dbName:'admin'
-}
+  username: 'root',
+  password: 'P@ssw0rd',
+  baseUrl: '159.65.4.14',
+  port: '2277',
+  dbName: 'test_db'
+};
 
-export default config
+export default config;
--- connect.js
+++ connect.js
@@ -2,8 +2,14 @@ import mongoos from 'mongoose';
 import config from './config';
 
 const { username, password, baseUrl, port, dbName } = config;
+const newPssword = encodeURIComponent(password);
+// DeprecationWarning: current Server Discovery and Monitoring engine is
+// deprecated, and will be removed in a future version. To use the new Server
+// Discover and Monitoring engine, pass option { useUnifiedTopology: true } to
+// the MongoClient constructor.
+mongoos.set('useUnifiedTopology', true);
 mongoos.connect(
-  `mongodb://${username}:${password}@${baseUrl}:${port}/${dbName}`,
+  `mongodb://${username}:${newPssword}@${baseUrl}:${port}/${dbName}`,
   { useNewUrlParser: true }
 );
 mongoos.connection
